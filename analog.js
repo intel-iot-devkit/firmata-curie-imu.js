de16e6ddb8111205df65767c44c7a23581926a89
deadprogram <ron@hybridgroup.com>
2016-03-21 16:01:37 -0700

    Adds example that reads both analog and IMU
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ analog.js
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+var CURIE_IMU = 0x11,
+    CURIE_IMU_READ_TEMP = 0x02;
+
+var Board = require("firmata");
+
+Board.requestPort(function(error, port) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  var board = new Board(port.comName);
+  board.on("ready", function() {
+    console.log("Ready.");
+
+    board.pinMode(0, board.MODES.ANALOG);
+    board.analogRead(0, function(value) {
+      console.log("analog 0:", value);
+    });
+
+    board.sysexResponse(CURIE_IMU, function(data) {
+      var subcommand = data.shift();
+      if (subcommand === CURIE_IMU_READ_TEMP) {
+        console.log("CURIE_IMU_READ_TEMP", Board.decode(data));
+      }
+    });
+
+    setInterval(function() {
+      board.sysexCommand([CURIE_IMU, CURIE_IMU_READ_TEMP]);
+    }, 500);
+  });
+});
